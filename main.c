778088cb6c8da6e5c81e28d38a747f15a5a72a36
dbwpdls22 <dbwpdls22@naver.com>
2018-11-11 17:07:28 +0900

    1_1

--- main.c
+++ main.c
@@ -4,5 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	//빙고 테이블 함수
+	//빙고 테이블 현재 상황을 화면에 출력 (두개)
+	//내가 빙고 번호 입력 선택 
+	//컴퓨터가 랜덤으로 빙고 번호 선택 
+	//숫자를 입력받아 테이블 칸을 채움 
+	//빙고 줄수를 계 
+	
 	return 0;
 }
