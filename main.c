be8f2928cb69ca3be373b5f02b5d8c0836eaa8c3
dbwpdls22 <dbwpdls22@naver.com>
2018-11-11 17:26:10 +0900

    1_2

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	//빙고 테이블 함수
-	//빙고 테이블 현재 상황을 화면에 출력 (두개)
-	//내가 빙고 번호 입력 선택 
-	//컴퓨터가 랜덤으로 빙고 번호 선택 
-	//숫자를 입력받아 테이블 칸을 채움 
-	//빙고 줄수를 계 
+	int initiate_bingo()//빙고 테이블 함수
+	print_bingo()//빙고 테이블 현재 상황을 화면에 출력 (두개)
+	get_number_byMe()//내가 빙고 번호 입력 선택 
+	get_number_byCom()//컴퓨터가 랜덤으로 빙고 번호 선택 
+	process_bingo()//숫자를 입력받아 테이블 칸을 채움 
+	count_bingo()//빙고 줄수를 계산 
 	
 	return 0;
 }
