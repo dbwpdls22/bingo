174525d93b8aa033941733ef9fc182ced7d13fa5
dbwpdls22 <dbwpdls22@naver.com>
2018-11-13 17:07:07 +0900

    1_3

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int initiate_bingo()
+{
+	int s[N][N];//2차원 배열 	
 	
-	int initiate_bingo()//빙고 테이블 함수
-	print_bingo()//빙고 테이블 현재 상황을 화면에 출력 (두개)
-	get_number_byMe()//내가 빙고 번호 입력 선택 
-	get_number_byCom()//컴퓨터가 랜덤으로 빙고 번호 선택 
-	process_bingo()//숫자를 입력받아 테이블 칸을 채움 
-	count_bingo()//빙고 줄수를 계산 
+};
+
+int main(int argc, char *argv[]) {
+	int n;
+	
+	n = initiate_bingo();
 	
 	return 0;
 }
