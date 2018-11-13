f155f1eb86590a4c58388b0e9eabaf70e9e2f268
dbwpdls22 <dbwpdls22@naver.com>
2018-11-13 18:24:24 +0900

    1_4

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 5
+#define M 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int initiate_bingo()
 {
-	int i, j;
-	int s[N][N];//2차원 배열 	
+	int i,j,k;
+	int count=1;
+	int randx = rand() % N*N
+	int randy = rand() % N*N
+	
+	for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			s[i][j]=count++;
+		}
+	}
+	
+	for (k=0;k<N;k++){
+		for (i=0;i<N;i++){
+		    for(j=0;j<N;j++)
+	}
+	
+	return 0;
 	
 };
 
@@ -16,20 +32,4 @@ int main(int argc, char *argv[]) {
 	
 	n = initiate_bingo();
 	
-	int i,j;
-	
-	int a[3][5]={
-	{1,2,3,4,5},
-	{2,3,4,5,6},
-	{3,4,5,6,7}
-	};
-	
-	for (i=0;i<3;i++){
-		for(j=0;j<5;j++){
-			printf("a[%d][%d]=%d",i,j,a[i][j]);
-		}
-		printf("\n");
-	}
-	
-	return 0;
 }
