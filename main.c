82d3b274925c930e38d0282bcdaaf0edee84a9df
dbwpdls22 <dbwpdls22@naver.com>
2018-11-13 17:54:56 +0900

    1_3

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 int initiate_bingo()
 {
+	int i, j;
 	int s[N][N];//2차원 배열 	
 	
 };
@@ -14,6 +15,21 @@ int main(int argc, char *argv[]) {
 	int n;
 	
 	n = initiate_bingo();
+	
+	int i,j;
+	
+	int a[3][5]={
+	{1,2,3,4,5},
+	{2,3,4,5,6},
+	{3,4,5,6,7}
+	};
+	
+	for (i=0;i<3;i++){
+		for(j=0;j<5;j++){
+			printf("a[%d][%d]=%d",i,j,a[i][j]);
+		}
+		printf("\n");
+	}
 	
 	return 0;
 }
