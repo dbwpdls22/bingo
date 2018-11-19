81ce1fe89e39035a890b7db32055e16f664ad2a8
dbwpdls22 <dbwpdls22@naver.com>
2018-11-19 22:03:58 +0900

    1_9

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int s[N][N];
 int i,j,k,temp;
 
-int initiate_bingo()
+int initiate_bingo1()
 {
 	srand(time(NULL));
 	int s[N][N];
@@ -34,24 +34,52 @@ int initiate_bingo()
 		    s[randx][j]=temp;
 	}
 }
-}
-
-int print_bingo(){
-	
-		for (i=0;i<N;i++){
+    for (i=0;i<N;i++){
 		for(j=0;j<N;j++){
 			printf(" %2d ", s[i][j]);
 		}
 		printf("\n");
-	
 }
-int get_number_byMe(){
-	
 }
+int initiate_bingo2()
+{
+	srand(time(NULL));
+	int s[N][N];
+	int i,j,k,temp;
+	int count=1;
+	int randx=1+ (rand() % N);
+	int randy=1+ (rand() % N);
+	
+	for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			s[i][j]=count++;
+		}
+	}
+	
+	for(k=0;k<N+1;k++){
+	for(i=0;i<N;i++){
+	    for(j=0;j<N;j++){
+		    temp=s[i][j];
+		    s[i][j]=s[i][randy];
+		    s[i][randy]=temp;
+		}
+		temp=s[i][j];
+		    s[i][j]=s[randx][j];
+		    s[randx][j]=temp;
+	}
+}
+    for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			printf(" %2d ", s[i][j]);
+		}
+		printf("\n");
+}
+}
+	
 int main(void){
-	
-	initiate_bingo();
-	print_bingo();
-	
+	initiate_bingo1();
+	printf("   <나의 빙고판>\n");
+	initiate_bingo2();
+	printf("   <상대의 빙고판>\n");
 	return 0;
 }
