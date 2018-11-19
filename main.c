dbb683439aa9fcf8917b6cafe168436146884e61
dbwpdls22 <dbwpdls22@naver.com>
2018-11-19 20:52:12 +0900

    1_6

--- main.c
+++ main.c
@@ -1,50 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 5
-#define M 5
+#include <time.h>
+#define N 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int initiate_bingo()
 {
-	srand(time(NULL));
-	int s[N][N];
+
+}
+int main(){
+	int MAX=25;
+	srand((unsigned)time(NULL));
+	int s[5][5];
 	int i,j,k,temp;
 	int count=1;
-	int randx = rand() % N*N;
-	int randy = rand() % N*N;
+	int randx=1+ (rand() % MAX);
+	int randy=1+ (rand() % MAX);
 	
-	for (i=0;i<N;i++){
-		for(j=0;j<N;j++){
+	for (i=0;i<5;i++){
+		for(j=0;j<5;j++){
 			s[i][j]=count++;
 		}
 	}
 	
-	for (k=0;k<N;k++){
-		for (i=0;i<N;i++){
-		    for(j=0;j<N;j++){
-		    	temp = s[i][j];
-		    	s[i][j] = s[randx][randy];
-		    	s[randx][randy]=temp;
-			}
-		    
+	for (k=0;k<10;k++){
+		for(i=0;i<5;i++){
+			temp=s[i][j];
+			s[i][j]=s[randx][j];
+			s[randx][j]=temp;
+			for(j=0;j<5;j++){
+			temp=s[i][j];
+			s[i][j]=s[i][randy];
+			s[i][randy]=temp;
+		    }
+		}
 	}
-	for (i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			printf("%2d",s[i][j]);
+	for (i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			printf("%2d", s[i][j]);
 		}
 		printf("\n");
 	}
-	return count;
-	
-};
-
-int main(int argc, char *argv[]) {
-	
-	
-	int initiate_bingo();
-	
-	printf("%d",initiate_bingo);
 	
 	return 0;
 }
