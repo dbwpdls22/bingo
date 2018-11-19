d78e1fef4f607653763f8f4f34d4f83ed8e24831
dbwpdls22 <dbwpdls22@naver.com>
2018-11-19 21:55:54 +0900

    1_8

--- main.c
+++ main.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 
+#define N 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int s[N][N];
+int i,j,k,temp;
 
 int initiate_bingo()
 {
-
-}
-int main(){
 	srand(time(NULL));
-	int s[5][5];
+	int s[N][N];
 	int i,j,k,temp;
 	int count=1;
-	int randx=1+ (rand() % 5);
-	int randy=1+ (rand() % 5);
+	int randx=1+ (rand() % N);
+	int randy=1+ (rand() % N);
 	
-	for (i=0;i<5;i++){
-		for(j=0;j<5;j++){
+	for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
 			s[i][j]=count++;
 		}
 	}
 	
-	for(k=0;k<5;k++){
-	for(i=0;i<5;i++){
-	    for(j=0;j<5;j++){
+	for(k=0;k<N;k++){
+	for(i=0;i<N;i++){
+	    for(j=0;j<N;j++){
 		    temp=s[i][j];
 		    s[i][j]=s[i][randy];
 		    s[i][randy]=temp;
@@ -35,13 +34,24 @@ int main(){
 		    s[randx][j]=temp;
 	}
 }
+}
+
+int print_bingo(){
 	
-	for (i=0;i<5;i++){
-		for(j=0;j<5;j++){
+		for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
 			printf(" %2d ", s[i][j]);
 		}
 		printf("\n");
-	}
+	
+}
+int get_number_byMe(){
+	
+}
+int main(void){
+	
+	initiate_bingo();
+	print_bingo();
 	
 	return 0;
 }
