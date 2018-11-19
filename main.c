d9f1289c0428d645f6ce69fbf152e24231902703
dbwpdls22 <dbwpdls22@naver.com>
2018-11-19 21:21:17 +0900

    1_7

--- main.c
+++ main.c
@@ -10,13 +10,12 @@ int initiate_bingo()
 
 }
 int main(){
-	int MAX=25;
-	srand((unsigned)time(NULL));
+	srand(time(NULL));
 	int s[5][5];
 	int i,j,k,temp;
 	int count=1;
-	int randx=1+ (rand() % MAX);
-	int randy=1+ (rand() % MAX);
+	int randx=1+ (rand() % 5);
+	int randy=1+ (rand() % 5);
 	
 	for (i=0;i<5;i++){
 		for(j=0;j<5;j++){
@@ -24,21 +23,22 @@ int main(){
 		}
 	}
 	
-	for (k=0;k<10;k++){
-		for(i=0;i<5;i++){
-			temp=s[i][j];
-			s[i][j]=s[randx][j];
-			s[randx][j]=temp;
-			for(j=0;j<5;j++){
-			temp=s[i][j];
-			s[i][j]=s[i][randy];
-			s[i][randy]=temp;
-		    }
+	for(k=0;k<5;k++){
+	for(i=0;i<5;i++){
+	    for(j=0;j<5;j++){
+		    temp=s[i][j];
+		    s[i][j]=s[i][randy];
+		    s[i][randy]=temp;
 		}
+		temp=s[i][j];
+		    s[i][j]=s[randx][j];
+		    s[randx][j]=temp;
 	}
+}
+	
 	for (i=0;i<5;i++){
 		for(j=0;j<5;j++){
-			printf("%2d", s[i][j]);
+			printf(" %2d ", s[i][j]);
 		}
 		printf("\n");
 	}
