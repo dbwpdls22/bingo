287644ae7ce48f097fd7b97a4b92867ff1ca2944
dbwpdls22 <dbwpdls22@naver.com>
2018-11-19 22:53:34 +0900

    1_9

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 #define N 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int s[N][N];
+int s1[N][N];
+int s2[N][N];
 int i,j,k,temp;
+int number;
 
 int initiate_bingo1()
 {
 	srand(time(NULL));
-	int s[N][N];
+	int s1[N][N];
 	int i,j,k,temp;
 	int count=1;
 	int randx=1+ (rand() % N);
@@ -18,33 +20,34 @@ int initiate_bingo1()
 	
 	for (i=0;i<N;i++){
 		for(j=0;j<N;j++){
-			s[i][j]=count++;
+			s1[i][j]=count++;
 		}
 	}
 	
 	for(k=0;k<N;k++){
 	for(i=0;i<N;i++){
 	    for(j=0;j<N;j++){
-		    temp=s[i][j];
-		    s[i][j]=s[i][randy];
-		    s[i][randy]=temp;
+		    temp=s1[i][j];
+		    s1[i][j]=s1[i][randy];
+		    s1[i][randy]=temp;
 		}
-		temp=s[i][j];
-		    s[i][j]=s[randx][j];
-		    s[randx][j]=temp;
+		temp=s1[i][j];
+		    s1[i][j]=s1[randx][j];
+		    s1[randx][j]=temp;
 	}
 }
     for (i=0;i<N;i++){
 		for(j=0;j<N;j++){
-			printf(" %2d ", s[i][j]);
+			printf(" %2d ", s1[i][j]);
 		}
 		printf("\n");
 }
+printf("   <나의 빙고판>\n");
 }
 int initiate_bingo2()
 {
 	srand(time(NULL));
-	int s[N][N];
+	int s2[N][N];
 	int i,j,k,temp;
 	int count=1;
 	int randx=1+ (rand() % N);
@@ -52,34 +55,75 @@ int initiate_bingo2()
 	
 	for (i=0;i<N;i++){
 		for(j=0;j<N;j++){
-			s[i][j]=count++;
+			s2[i][j]=count++;
 		}
 	}
 	
 	for(k=0;k<N+1;k++){
 	for(i=0;i<N;i++){
 	    for(j=0;j<N;j++){
-		    temp=s[i][j];
-		    s[i][j]=s[i][randy];
-		    s[i][randy]=temp;
+		    temp=s2[i][j];
+		    s2[i][j]=s2[i][randy];
+		    s2[i][randy]=temp;
 		}
-		temp=s[i][j];
-		    s[i][j]=s[randx][j];
-		    s[randx][j]=temp;
+		temp=s2[i][j];
+		    s2[i][j]=s2[randx][j];
+		    s2[randx][j]=temp;
 	}
 }
     for (i=0;i<N;i++){
 		for(j=0;j<N;j++){
-			printf(" %2d ", s[i][j]);
+			printf(" %2d ", s2[i][j]);
 		}
 		printf("\n");
 }
+printf("   <상대의 빙고판>\n");
+}
+int get_number_byMe()
+{
+	int number;
+	printf("빙고 번호를 입력하시오: ");
+	scanf("%d",&number);
+ } 
+int erase_bingo(int s1[N][N],int s2[N][N], int number){
+    int x,y; 
+	
+	for (x=0;x<N;x++){
+		for(y=0;y<N;y++){
+			if(s1[x][y]==number){
+				s1[x][y]==0;
+			}
+			   
+		}
+	}
+	for (x=0;x<N;x++){
+		for(y=0;y<N;y++){
+			if(s2[x][y]==number){
+				s2[x][y]==0;
+			}
+			   
+		}
+	}
+	for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			printf(" %2d ", s1[i][j]);
+		}
+		printf("\n");
+	}
+	printf("   <나의 빙고판>\n");
+		for (i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			printf(" %2d ", s2[i][j]);
+		}
+		printf("\n");
+}
+    printf("   <상대의 빙고판>\n");
 }
 	
 int main(void){
 	initiate_bingo1();
-	printf("   <나의 빙고판>\n");
 	initiate_bingo2();
-	printf("   <상대의 빙고판>\n");
+	get_number_byMe();
+	erase_bingo(s1[N][N],s2[N][N],number);
 	return 0;
 }
