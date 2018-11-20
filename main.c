f7d675e855e77825d1c8ed1b50eb6befdb666bb7
dbwpdls22 <dbwpdls22@naver.com>
2018-11-21 02:24:25 +0900

    2_2

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define N 5
 #define M 25//N*N
 
+int Mybingo[N][N];
+int Combingo[N][N];
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y){
+void swap(int *x, int *y){       //
 	int temp;
 	temp=*x;
 	*x=*y;
@@ -21,4 +24,23 @@ void initiate_bingo(int *array){
 	for(i=0;i<M;i++){
 		swamp(&array[i],&array[rand()%M]);
 	}
+}
+
+void print_bingo(int arr[N][N]){
+	int x,y;
+	
+	for (y=0;y<N;y++){
+       for(x=0;x<N;x++){
+       	printf("%7d",arr[x][y]);
+        }
+        printf("\n\n");
+    }
+}
+
+void main(){
+	
+	initiate_bingo((int*)Mybingo);
+	print_bingo(Mybingo);
+	
+    return 0;
 }
